a9ceca524b554abcd96ef68974ca6178666eb8e3
kossowski <artosz.kossowski@forum-media.pl>
2022-01-28 14:44:37 +0100

    trzeci

--- app.py
+++ app.py
@@ -1,16 +1,76 @@
-import required as required
-from flask import Flask, render_template, jsonify, make_response
+# import required as required
+from flask import Flask, render_template, make_response, redirect, url_for, flash
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
+from flask_wtf import FlaskForm
 from time import time
 from random import random
 import json
 from flask_sqlalchemy import SQLAlchemy
-
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, login_user, login_manager, UserMixin, logout_user
 
 app = Flask(__name__)
 api = Api(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'a8867c185f9a2d2c2d47f5a2'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(length=70), nullable=False, unique=True)
+    email_address = db.Column(db.String(length=70), nullable=False, unique=True)
+    password_hash = db.Column(db.String(length=70), nullable=False)
+    budget = db.Column(db.Integer(), nullable=False, default=1000)
+
+    @property
+    def password(self):
+        return self.password
+
+    @password.setter
+    def password(self, plain_text_password):
+        self.password_hash = bcrypt.generate_password_hash(plain_text_password).decode('utf-8')
+
+    def check_password_correcting(self, attempted_password):
+        return bcrypt.check_password_hash(self.password_hash, attempted_password)
+
+
+class RegisterForm(FlaskForm):
+
+    def validate_username(self, username_to_check):
+        user = User.query.filter_by(username=username_to_check.data).first()
+        if user:
+            raise ValidationError('Użytkownik już istnieje! Wpisz inną nazwę...')
+
+    def  validate_email_address(self, email_address_to_check):
+        email_address = User.query.filter_by(email_address=email_address_to_check.data).first()
+        if email_address:
+            raise ValidationError('Adres email już istieje w bazie, jeśli to Twój to napisz do admina [bartosz.kossowski@forum-media.pl]')
+
+    username = StringField(label='Nazwa użytkownika', validators=[Length(min=4, max=30), DataRequired()])
+    email_address = StringField(label='Adres email', validators=[Email(), DataRequired()])
+    password1 = PasswordField(label='Wprowadź hasło', validators=[DataRequired()])
+    password2 = PasswordField(label='Powtórz hasło', validators=[EqualTo('password1'), DataRequired()])
+    submit = SubmitField(label='Zarejestruj')
+
+
+class LoginForm(FlaskForm):
+    username = StringField(label='Nazwa Użytkownika', validators=[DataRequired()])
+    password = PasswordField(label='Hasło', validators=[DataRequired()])
+    submit = SubmitField(label='Zaloguj')
+
+
+# db.create_all()
 
 
 class databaseModel(db.Model):
@@ -59,6 +119,7 @@ class alliwantWRONG(db.Model):
 # pierdolony createall 3eba zawsze odkomentować iedy dodaje się nowy model bazy dla 'utworzenia jej'
 #db.create_all()
 
+
 dane_put_args = reqparse.RequestParser()
 dane_put_args.add_argument("name", type=str, help="pomoc dla powodzian", required=True) # to jest wymagane, tak jakby tutaj wypisuje się wymagane argument
 dane_put_args.add_argument("slogan", type=str, help="zlomowsiko", required=True) # tu też jak wyżej, jeżeli nie ma czegoś w PUTie to wyświetlany jest help
@@ -99,6 +160,8 @@ fraud_url = []
 #####
 
 # wyjątki i to co ma być, czyli informacja o tym co jest a czego nie ma ;)
+
+
 def abort_nie_ma(dane):
     if dane not in data:
         abort(404, message="Nie ma takego rekordu dzbanie")
@@ -134,12 +197,51 @@ def delete_record(num):
     db.session.commit()
     return
 
+
 @app.route('/setData', methods=["GET", "POST"])
 def setData():
     data = [time() * 1000, random() * 100]
     response = make_response(json.dumps(data))
     response.content_type = 'application/json'
     return response
+
+
+@app.route('/rejestracja_1e8a9ae72c905a68d6de2c30', methods=['GET', 'POST'])
+def regist_er():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user_to_create = User(username=form.username.data,
+                              email_address=form.email_address.data,
+                              password=form.password1.data)
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for('login_page'))
+    if form.errors != {}: # if are not errors from validator
+        for err_msg in form.errors.values():
+            flash(f'Wystapił błąd przy próbie rejestracji: {err_msg}', category='danger')
+    return render_template('register.html', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_page():
+    form = LoginForm()
+    if form.validate_on_submit():
+        attemped_user = User.query.filter_by(username=form.username.data).first()
+        if attemped_user and attemped_user.check_password_correcting(attempted_password=form.password.data):
+            login_user(attemped_user)
+            flash(f'Udało się zalogować do systemu', category='success')
+            return redirect(url_for('home_page'))
+        else:
+            flash(f'Nazwa użytkownika lub hasło jest nie prawidłowe...', category='danger')
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+def logout_page():
+    logout_user()
+    flash("Zostałaś/eś wylogowana/ny", category='info')
+    return redirect(url_for('login_page'))
+
 
 nazwy = {'Bartek': {'age': 24, 'gender': 'male'},
          'Monika': {'age': 29, 'gender': 'female'},
@@ -297,10 +399,10 @@ class all_i_want_WRONG(Resource):
         db.session.commit()
         return miarka, 201
 
-    def delete(self, num):
-        result = alliwantWRONG.query.filter_by(id=num).one()
-        db.session.delete(result)
-        db.session.commit()
+    # def delete(self, num):
+    #     result = alliwantWRONG.query.filter_by(id=num).one()
+    #     db.session.delete(result)
+    #     db.session.commit()
 
 api.add_resource(HelloWorld, "/toja")
 api.add_resource(api2, '/aaa')
